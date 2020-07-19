f8ba70c5c9659f932100b144cdcef1bdd0263d7d
Virendrasingh Rajput <rvirendra17@gmail.com>
2020-07-19 16:18:18 +0530

    Book Validation Constant messages reformatted

--- src/main/java/com/abc/lib/constants/ValidationConstant.java
+++ src/main/java/com/abc/lib/constants/ValidationConstant.java
@@ -2,11 +2,11 @@ package com.abc.lib.constants;
 
 public class ValidationConstant {
 
-	public static final String Validation_BookTitle_Message = "REQUIRED VALUE: Title of book is missing or empty";
-	public static final String Validation_BookSubTitle_Message = "null value is not accepted for: Subtitle of book";
-	public static final String Validation_BookAuthor_Message = "REQUIRED VALUE: Author of book is missing or empty";
-	public static final String Validation_BookEdition_Message = "REQUIRED VALUE: Edition of book is missing or empty";
-	public static final String Validation_BookPublisher_Message = "REQUIRED VALUE: Publisher of book is missing or empty";
+	public static final String Validation_BookTitle_Message = "REQUIRED VALUE: title of book is missing or empty";
+	public static final String Validation_BookSubTitle_Message = "sub_title=null, 'null' value is not accepted";
+	public static final String Validation_BookAuthor_Message = "REQUIRED VALUE: author of book is missing or empty";
+	public static final String Validation_BookEdition_Message = "REQUIRED VALUE: edition of book is missing or empty";
+	public static final String Validation_BookPublisher_Message = "REQUIRED VALUE: publisher of book is missing or empty";
 
 	private ValidationConstant() {
 		// TODO Auto-generated constructor stub
